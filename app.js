f59fed77760af2bb48830cb6e1b2faed15ad2922
axell.bustos <axell.bustos@hotmail.com.ar>
2022-07-27 20:14:19 -0300

    cambios en configuracion de puertos

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+require("dotenv").config();
 const express=require('express');
 const app=express();
 const path=require('path');
+
 
 app.use(express.static('public'));
 
 app.get('/',(req,res)=>res.sendFile(path.join(__dirname, 'views', 'home.html')))
 app.get('/',(req,res)=>{res.send()});
-app.listen(3030,()=>console.log('servidor corriendo'));
+app.listen(process.env.PORT || 3030,()=>console.log('servidor corriendo'));
