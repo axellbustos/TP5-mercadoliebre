e7b98b1e44b98cf1b00c9108fe7e83ec5486c6ff
axellbustos <axell.bustos@hotmail.com.ar>
2022-08-06 15:30:41 -0300

    nuevos estilos y nuevos campos

--- app.js
+++ app.js
@@ -7,5 +7,6 @@ const path=require('path');
 app.use(express.static('public'));
 
 app.get('/',(req,res)=>res.sendFile(path.join(__dirname, 'views', 'home.html')))
+app.get('/register',(req,res)=>res.sendFile(path.join(__dirname, 'views', 'register.html')))
 app.get('/',(req,res)=>{res.send()});
 app.listen(process.env.PORT || 3030,()=>console.log('servidor corriendo'));
